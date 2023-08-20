9f548cf89f6aab1c05d9d0d8e1802cddc5c3f4b7
shafeequca <shafeequ.ca@gmail.com>
2023-08-20 10:52:32 +0530

    Initaial Setup

---
+++ InMag V.16/DataSet/DataSet1.cs
@@ -0,0 +1,14 @@
+﻿namespace InMag_V._16.DataSet
+{
+
+
+    public partial class DataSet1
+    {
+    }
+}
+namespace InMag_V._16.DataSet {
+    
+    
+    public partial class DataSet1 {
+    }
+}
---
+++ script.sql
@@ -0,0 +1,5 @@
+USE [Inventory_GST_Purchase_Banana]
+GO
+alter table tblSales ADD [Rent] [float] NULL
+alter table tblSales ADD [Commission] [float] NULL
+alter table tblSales ADD [Cooli] [float] NULL
